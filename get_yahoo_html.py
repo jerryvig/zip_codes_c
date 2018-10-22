a81a4f2fd234b0b5b143e643f2ee7d605ef20759
Jerry Vigil <jerry.vigil@mktneutral.com>
2018-10-22 17:28:07 -0500

    updates.

--- get_yahoo_html.py
+++ get_yahoo_html.py
@@ -3,6 +3,23 @@ from xml.dom import minidom
 import sys
 import requests
 
+def getTbodyNode(dom):
+    for node in dom.documentElement.childNodes:
+        if node.tagName == 'tbody':
+    	    return node
+
+def getAdjClosePrices(tbody):
+    adjClosePrices = []
+    for node in tbody.childNodes:
+        if node.tagName == 'tr':
+            tdCount = 0
+            for child in node.childNodes:
+                if child.tagName == 'td':
+                    tdCount += 1
+                    if tdCount == 6:
+                        for span in child.childNodes:
+                            adjClosePrices.append(span.childNodes[0].toxml().strip())
+    return adjClosePrices
 
 def main():
     for arg in sys.argv:
@@ -13,10 +30,13 @@ def main():
         "<table class=\"W(100%) M(0)\" data-test=\"historical-prices\"")
     table_start = response.text[table_start_idx:]
     table_end_idx = table_start.find("<div class=")
-    print(table_start[:table_end_idx])
 
     dom = minidom.parseString(table_start[:table_end_idx])
-    print(dom.documentElement.tagName)
+    tbody = getTbodyNode(dom)
+
+
+    adjPrices = getAdjClosePrices(tbody)
+    print(adjPrices)
 
 if __name__ == '__main__':
     main()
