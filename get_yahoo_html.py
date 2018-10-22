ed06eda79d2d8e59a59f2ddb93858d2bca8ff242
Jerry Vigil <jerry.vigil@mktneutral.com>
2018-10-22 16:33:02 -0500

    updates

--- get_yahoo_html.py
+++ get_yahoo_html.py
@@ -1,24 +1,15 @@
 import requests
 import sys
 
-from html.parser import HTMLParser
-
-class YahooParser(HTMLParser):
-	def handle_starttag(self, tag, attrs):
-		if tag == 'table':
-			for attr in attrs:
-				if attr[0] == 'data-test':
-					pass
-
 def main():
 	for arg in sys.argv:
 		print('arg = {}'.format(arg))
 
 	response = requests.get('https://finance.yahoo.com/quote/SHOP/history?p=SHOP')
-	# print(response.text)
-
-	parser = YahooParser()
-	parser.feed(response.text)
+	table_start_idx = response.text.find("<table class=\"W(100%) M(0)\" data-test=\"historical-prices\"")
+	table_start = response.text[table_start_idx:]
+	table_end_idx = table_start.find("<div class=")
+	print(table_start[:table_end_idx])
 	
 if __name__ == '__main__':
 	main()
