072fb3ad3aeda60c2ea66213762fd566753fe6d2
Jerry Vigil <jerry.vigil@mktneutral.com>
2018-10-22 16:53:41 -0500

    updates.

--- get_yahoo_html.py
+++ get_yahoo_html.py
@@ -1,15 +1,22 @@
-import requests
+from xml.dom import minidom
+
 import sys
+import requests
+
 
 def main():
-	for arg in sys.argv:
-		print('arg = {}'.format(arg))
+    for arg in sys.argv:
+        print('arg = {}'.format(arg))
 
-	response = requests.get('https://finance.yahoo.com/quote/SHOP/history?p=SHOP')
-	table_start_idx = response.text.find("<table class=\"W(100%) M(0)\" data-test=\"historical-prices\"")
-	table_start = response.text[table_start_idx:]
-	table_end_idx = table_start.find("<div class=")
-	print(table_start[:table_end_idx])
-	
+    response = requests.get('https://finance.yahoo.com/quote/SHOP/history?p=SHOP')
+    table_start_idx = response.text.find(
+        "<table class=\"W(100%) M(0)\" data-test=\"historical-prices\"")
+    table_start = response.text[table_start_idx:]
+    table_end_idx = table_start.find("<div class=")
+    print(table_start[:table_end_idx])
+
+    dom = minidom.parseString(table_start[:table_end_idx])
+    print(dom.documentElement.tagName)
+
 if __name__ == '__main__':
-	main()
+    main()
