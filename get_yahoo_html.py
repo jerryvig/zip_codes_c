dba2535cbf8086f5cde2ee58be4d0d20c6d9ba36
Jerry Vigil <jerry.vigil@mktneutral.com>
2018-10-23 15:53:23 -0500

    updates

--- get_yahoo_html.py
+++ get_yahoo_html.py
@@ -1,21 +1,22 @@
 from xml.dom import minidom
 
 import sys
+import time
 import requests
 
-def getTableDom(response):
+def get_table_dom(response):
     table_start_idx = response.text.find(
         "<table class=\"W(100%) M(0)\" data-test=\"historical-prices\"")
     table_start = response.text[table_start_idx:]
     table_end_idx = table_start.find("<div class=")
     return minidom.parseString(table_start[:table_end_idx])
 
-def getTbodyNode(dom):
+def get_tbody_node(dom):
     for node in dom.documentElement.childNodes:
         if node.tagName == 'tbody':
             return node
 
-def getAdjClosePrices(tbody):
+def get_adj_close(tbody):
     adj_close_prices = []
     for node in tbody.childNodes:
         if node.tagName == 'tr':
@@ -33,14 +34,15 @@ def main():
         print('No ticker argument supplied. Exiting....')
         return
 
-    ticker = sys.argv[1].strip()
-    url = 'https://finance.yahoo.com/quote/%s/history?p=%s' % (ticker, ticker)
-    response = requests.get(url)
-    dom = getTableDom(response)
-    tbody = getTbodyNode(dom)
-
-    adj_prices = getAdjClosePrices(tbody)
-    print(adj_prices)
+    for tick in sys.argv[1:]:
+        ticker = tick.strip()
+        url = 'https://finance.yahoo.com/quote/%s/history?p=%s' % (ticker, ticker)
+        response = requests.get(url)
+        dom = get_table_dom(response)
+        tbody = get_tbody_node(dom)
+        adj_prices = get_adj_close(tbody)
+        print(adj_prices)
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
